dc380e820eb65f9cb9008e385fe8f3e66a95111d
ramirocadavid <ramirocadavid@gmail.com>
2017-12-06 10:01:40 -0500

    Initial commit with script

---
+++ IVR sampling.R
@@ -0,0 +1,11 @@
+
+# Calculate sample size -------------------------------------------------------------
+
+
+
+
+# Select sample ---------------------------------------------------------------------
+
+# https://www.rdocumentation.org/packages/fifer/versions/1.0/topics/stratified
+source("stratified.R")
+
